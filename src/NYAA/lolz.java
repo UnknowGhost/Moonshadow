ba1873fc7fa1416e1289305ff496a159dc04acb9
Justin <justin-z@live.nl>
2017-09-05 09:26:54 +0200

    naam met kleur

--- src/NYAA/lolz.java
+++ src/NYAA/lolz.java
@@ -1,4 +1,33 @@
-package NYAA;
+package NYAA;/*
 
-public class lolz {
+@(#)Show.java 1.0 03/02/26
+
+*
+
+Voorbeeld 2.1
+
+*
+
+*/
+
+//importeer de benodigde klassen uit de Java bibliotheek
+import java.awt.*;
+import java.applet.*;
+
+//een klasse met de naam Show van het type Applet
+public class lolz extends Applet {
+
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+        setBackground(Color.black);
+
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("sinia", 50, 60 );
+        g.setColor(Color.pink);
+        g.drawString("desert", 82, 60 );
+    }
+
 }
