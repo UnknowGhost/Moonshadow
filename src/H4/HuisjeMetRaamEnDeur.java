21abf823d1ddae8f15743ac3b8e3ca9023ae5b23
Justin <justin-z@live.nl>
2017-09-06 11:41:28 +0200

    huisje met raam

--- src/H4/HuisjeMetRaamEnDeur.java
+++ src/H4/HuisjeMetRaamEnDeur.java
@@ -13,6 +13,9 @@ public class HuisjeMetRaamEnDeur extends Applet {
         g.drawRect(120, 150, 120, 80);
         g.drawRect(140, 190, 30, 20);
         g.drawLine(240, 150, 180, 50);
+        g.drawLine(120, 150,180,50);
+        g.drawRect(210, 180, 20, 50);
+
     }
 }
 
