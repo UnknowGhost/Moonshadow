a5034be6efd500cc6b3a3814c13d02023e476abe
Justin <justin-z@live.nl>
2017-09-06 22:10:06 +0200

    diagram

--- src/H4/Diagram.java
+++ src/H4/Diagram.java
@@ -1,4 +1,20 @@
 package H4;
 
-public class Diagram {
+import java.awt.*;
+import java.applet.*;
+
+public class Diagram extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+        g.setColor(Color.black);
+        g.drawLine(20, 180, 180, 180);
+        g.drawLine(20, 20,20,180);
+        
+
+    }
+
 }
