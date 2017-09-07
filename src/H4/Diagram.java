9cb1c4f1b81d4c05028989915a7ef2c66e937b69
Justin <justin-z@live.nl>
2017-09-07 10:15:35 +0200

    diagram

--- src/H4/Diagram.java
+++ src/H4/Diagram.java
@@ -11,8 +11,16 @@ public class Diagram extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.white);
         g.setColor(Color.black);
-        g.drawLine(20, 180, 180, 180);
-        g.drawLine(20, 20,20,180);
+        g.drawLine(30, 180, 200, 180);
+        g.drawLine(30, 50,30,180);
+        g.drawString("0", 20, 180);
+        g.drawString("20",15, 160);
+        g.drawString("40",15, 140);
+        g.drawString("60",15, 120);
+        g.drawString("80",15, 100);
+        g.drawString("100", 10, 80);
+        g.drawString("120", 10, 60);
+
 
     }
 
