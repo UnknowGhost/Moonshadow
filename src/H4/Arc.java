05413e7549e7313ac83949a4be8f9f054f048a3c
Justin <justin-z@live.nl>
2017-09-08 17:05:21 +0200

    arc

---
+++ src/H4/Arc.java
@@ -0,0 +1,17 @@
+package H4;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Arc extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+      setBackground(Color.blue);
+      g.setColor(Color.yellow);
+      g.fillArc(100,100,150,130,0,360);
+
+    }
+}
