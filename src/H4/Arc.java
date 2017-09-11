27b005e2cdea1406d02a8632c8a7757d85deab80
Justin <justin-z@live.nl>
2017-09-11 12:56:25 +0200

    praktijkopdracht

--- src/H4/Arc.java
+++ src/H4/Arc.java
@@ -1,17 +1,18 @@
 package H4;
-
 import java.awt.*;
 import java.applet.*;
 
 public class Arc extends Applet {
 
-    public void init() {
-    }
+
+
 
     public void paint(Graphics g) {
       setBackground(Color.blue);
+
       g.setColor(Color.yellow);
       g.fillArc(100,100,150,130,0,360);
 
     }
 }
+
