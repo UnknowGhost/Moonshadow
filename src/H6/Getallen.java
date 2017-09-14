e1c6eccdaecf5bfba0bb5101ac711c62b9b21ada
Justin <justin-z@live.nl>
2017-09-14 13:25:49 +0200

    Getallen h06

---
+++ src/H6/Getallen.java
@@ -0,0 +1,29 @@
+package H6;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Getallen extends Applet{
+    int a;
+    int b;
+
+
+
+    public void init(){
+        a = 10;
+        b = 20;
+
+
+
+    }
+
+    public void paint(Graphics graphics){
+
+        graphics.drawString("de uitkomst is: " + - (a + b), 50, 50);
+    }
+
+
+
+
+
+}
